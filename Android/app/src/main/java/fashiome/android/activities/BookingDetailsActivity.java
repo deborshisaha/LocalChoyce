5eb6ce54a75df362a83b7fc3a4bb66ccec3102ce
Rakhee Gupta <Rakhee Gupta>
2016-03-12 17:47:08 -0800

    add credit card reader card.io

--- Android/app/src/main/java/fashiome/android/activities/BookingDetailsActivity.java
+++ Android/app/src/main/java/fashiome/android/activities/BookingDetailsActivity.java
@@ -7,10 +7,15 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
 import fashiome.android.R;
+import io.card.payment.CardIOActivity;
+import io.card.payment.CreditCard;
 
 public class BookingDetailsActivity extends AppCompatActivity {
+
+    private int MY_SCAN_REQUEST_CODE = 100; // arbitrary int
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,4 +41,55 @@ public class BookingDetailsActivity extends AppCompatActivity {
         Intent i = new Intent(BookingDetailsActivity.this, PaymentActivity.class);
         startActivity(i);
     }
+
+    public void onScanPress(View v) {
+        Intent scanIntent = new Intent(this, CardIOActivity.class);
+
+        // customize these values to suit your needs.
+        scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_EXPIRY, true); // default: false
+        scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_CVV, false); // default: false
+        scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_POSTAL_CODE, false); // default: false
+
+        // MY_SCAN_REQUEST_CODE is arbitrary and is only used within this activity.
+        startActivityForResult(scanIntent, MY_SCAN_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == MY_SCAN_REQUEST_CODE) {
+            String resultDisplayStr;
+            if (data != null && data.hasExtra(CardIOActivity.EXTRA_SCAN_RESULT)) {
+                CreditCard scanResult = data.getParcelableExtra(CardIOActivity.EXTRA_SCAN_RESULT);
+
+                // Never log a raw card number. Avoid displaying it, but if necessary use getFormattedCardNumber()
+                resultDisplayStr = "Card Number: " + scanResult.getRedactedCardNumber() + "\n";
+
+                // Do something with the raw number, e.g.:
+                // myService.setCardNumber( scanResult.cardNumber );
+
+                if (scanResult.isExpiryValid()) {
+                    resultDisplayStr += "Expiration Date: " + scanResult.expiryMonth + "/" + scanResult.expiryYear + "\n";
+                }
+
+                if (scanResult.cvv != null) {
+                    // Never log or display a CVV
+                    resultDisplayStr += "CVV has " + scanResult.cvv.length() + " digits.\n";
+                }
+
+                if (scanResult.postalCode != null) {
+                    resultDisplayStr += "Postal Code: " + scanResult.postalCode + "\n";
+                }
+            }
+            else {
+                resultDisplayStr = "Scan was canceled.";
+            }
+            // do something with resultDisplayStr, maybe display it in a textView
+            // resultTextView.setText(resultDisplayStr);
+            Toast.makeText(BookingDetailsActivity.this, "Congratulations!! Payment done", Toast.LENGTH_LONG).show();
+        }
+        // else handle other activity results
+    }
+
 }
