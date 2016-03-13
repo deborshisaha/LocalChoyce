7c2f60f3c5f0c866965b23cca0222776db80890e
Deepak Barge <deepak@sumologic.com>
2016-03-12 20:40:42 -0800

    - resolving merge conflicts with latest master - placeholder filter settings activity/fragment

---
+++ Android/app/src/main/java/fashiome/android/activities/ProductFilterSettingsActivity.java
@@ -0,0 +1,31 @@
+package fashiome.android.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import fashiome.android.R;
+
+public class ProductFilterSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_filter_settings);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
