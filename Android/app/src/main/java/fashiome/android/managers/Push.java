42337bedd4b3362f5587d10cd0f72ad9caa795df
Deborshi Saha <deborshi.saha@itsoninc.com>
2016-03-21 19:39:41 -0700

    Notification on renting opens UserDetailsActivity

--- Android/app/src/main/java/fashiome/android/managers/Push.java
+++ Android/app/src/main/java/fashiome/android/managers/Push.java
@@ -16,6 +16,11 @@ import fashiome.android.models.User;
  */
 public class Push {
 
+    public static String PUSH_TYPE = "PUSH_TYPE";
+
+    public static String PUSH_TYPE_LIKE = "PUSH_TYPE_LIKE";
+    public static String PUSH_TYPE_RENT = "PUSH_TYPE_RENT";
+
     public static void userLikedProduct(Product product) throws JSONException {
 
         ParseQuery pushQuery = ParseInstallation.getQuery();
@@ -26,11 +31,17 @@ public class Push {
 
         String message = currentUser.getUsername() + " liked "+ product.getProductName();
 
-        JSONObject data = new JSONObject("{\"alert\":\""+message+"\", \"data\":{\""+Product.PRODUCT_ID+"\":\""+ product.getObjectId() +"\"}}");
+        JSONObject dataObject = new JSONObject();
+        dataObject.put(PUSH_TYPE, PUSH_TYPE_LIKE);
+        dataObject.put(Product.PRODUCT_ID, product.getObjectId());
+
+        JSONObject parentJSONObject = new JSONObject();
+        parentJSONObject.put("alert", message);
+        parentJSONObject.put("data", dataObject);
 
         ParsePush push = new ParsePush();
         push.setQuery(pushQuery);
-        push.setData(data);
+        push.setData(parentJSONObject);
         push.sendInBackground();
     }
 
@@ -42,11 +53,17 @@ public class Push {
         User currentUser = (User) ParseUser.getCurrentUser();
         String message = currentUser.getUsername() + " rented "+ product.getProductName();
 
-        JSONObject data = new JSONObject("{\"alert\":\""+message+"\", \"data\":{\""+Product.PRODUCT_ID+"\":\""+ product.getObjectId() +"\"}}");
+        JSONObject dataObject = new JSONObject();
+        dataObject.put(PUSH_TYPE, PUSH_TYPE_RENT);
+        dataObject.put(Product.PRODUCT_ID, product.getObjectId());
+
+        JSONObject parentJSONObject = new JSONObject();
+        parentJSONObject.put("alert", message);
+        parentJSONObject.put("data", dataObject);
 
         ParsePush push = new ParsePush();
         push.setQuery(pushQuery);
-        push.setData(data);
+        push.setData(parentJSONObject);
         push.sendInBackground();
     }
 }
