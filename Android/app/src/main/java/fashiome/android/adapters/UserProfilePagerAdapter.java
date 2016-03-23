47081384850f68acb3a0a59e6ab3216ea08fa19e
Deepak Barge <deepak@sumologic.com>
2016-03-22 20:03:03 -0700

    Bug fix - Difference in the way user details page appears for owner and other user. Currently everyone can see each other's UserDetailActivity Bug fix - App when logged in first time from the tour screen crashes.

--- Android/app/src/main/java/fashiome/android/adapters/UserProfilePagerAdapter.java
+++ Android/app/src/main/java/fashiome/android/adapters/UserProfilePagerAdapter.java
@@ -17,19 +17,28 @@ import fashiome.android.models.User;
 
 public class UserProfilePagerAdapter extends FragmentPagerAdapter implements PagerSlidingTabStrip.IconTabProvider {
 
-    private String tabTitles[] = new String[] { "Items bought", "followers", "messages", "collections" };
+    private String tabTitles[];
 
-    private int tabIcons[] = {R.drawable.ic_shopped, R.drawable.ic_user_followers,
-            R.drawable.ic_user_messages, R.drawable.ic_other_collections};
+    private int tabIcons[] ;
 
     //private Context context;
+    private boolean isOwner;
 
     public User user;
 
-    public UserProfilePagerAdapter(FragmentManager fm, User user) {
+    public UserProfilePagerAdapter(FragmentManager fm, User user, boolean flag) {
         super(fm);
         //this.context = context;
         this.user = user;
+        this.isOwner = flag;
+        if(isOwner){
+            tabTitles = new String[] { "Items bought", "followers", "messages", "collections" };
+            tabIcons = new int[] {R.drawable.ic_shopped, R.drawable.ic_user_followers,
+                    R.drawable.ic_user_messages, R.drawable.ic_other_collections};
+        } else {
+            tabTitles = new String[] {"followers", "collections" };
+            tabIcons = new int[] {R.drawable.ic_user_followers, R.drawable.ic_other_collections};
+        }
     }
 
     @Override
@@ -40,29 +49,45 @@ public class UserProfilePagerAdapter extends FragmentPagerAdapter implements Pag
     @Override
     public Fragment getItem(int position) {
 
-        switch (position){
+        if(isOwner) {
+            switch (position) {
 
-            case 0:
-                return UserBoughtItemsFragment.newInstance(user);
-            //break;
+                case 0:
+                    return UserBoughtItemsFragment.newInstance(user);
+                //break;
 
-            case 1:
-                return UserFollowerListFragment.newInstance(user);
-            //break;
+                case 1:
+                    return UserFollowerListFragment.newInstance(user);
+                //break;
 
-            case 2:
-                return UserMessagesFragment.newInstance(user);
-            //break;
+                case 2:
+                    return UserMessagesFragment.newInstance(user);
+                //break;
 
-            case 3:
-                return UserMediaCollectionFragment.newInstance(user);
-            //break;
+                case 3:
+                    return UserMediaCollectionFragment.newInstance(user);
+                //break;
 
-            default:
-                return null;
-            //break;
+                default:
+                    return null;
+                //break;
+            }
+        } else {
+            switch (position) {
+
+                case 0:
+                    return UserFollowerListFragment.newInstance(user);
+                //break;
+
+                case 1:
+                    return UserMediaCollectionFragment.newInstance(user);
+                //break;
+
+                default:
+                    return null;
+                //break;
+            }
         }
-        //return (position == 0)? HomeTimelineTweetsFragment.newInstance(user.getUserId()) : UserTimelineMediaFragment.newInstance(user.getUserId()) ;
     }
 
     @Override
@@ -73,6 +98,9 @@ public class UserProfilePagerAdapter extends FragmentPagerAdapter implements Pag
     @Override
     public CharSequence getPageTitle(int position) {
 
+        return tabTitles[position];
+
+/*
         switch (position) {
             case 0:
                 return tabTitles[0];
@@ -89,5 +117,6 @@ public class UserProfilePagerAdapter extends FragmentPagerAdapter implements Pag
             default:
                 return "";
         }
+*/
     }
 }
--- Android/app/src/main/java/fashiome/android/utils/Utils.java
+++ Android/app/src/main/java/fashiome/android/utils/Utils.java
@@ -4,9 +4,16 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Point;
 import android.os.Build;
+import android.text.format.DateUtils;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Display;
 import android.view.WindowManager;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 /**
  * Created by dsaha on 3/5/16.
@@ -14,6 +21,8 @@ import android.view.WindowManager;
 public class Utils {
     private static int screenWidth = 0;
     private static int screenHeight = 0;
+
+    public static final String PARSE_DATE_FORMAT = "EEE MMM DDD HH:mm:ss z yyyy";
 
     public static int getScreenWidthInDp(Context c) {
 
@@ -62,6 +71,37 @@ public class Utils {
         return Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP;
     }
 
+    public static String getRelativeTimeAgo(String rawJsonDate) {
+        //SimpleDateFormat sf = new SimpleDateFormat(TWITTER_DATE_FORMAT, Locale.ENGLISH);
+        //sf.setLenient(true);
+
+        String relativeDate = "";
+
+        long dateMillis = convertToDate(rawJsonDate).getTime();
+
+        Log.i("info", "Millis createdAt :" + String.valueOf(dateMillis));
+
+        Log.i("info", "Millis now :"+String.valueOf(System.currentTimeMillis()));
+
+        relativeDate = DateUtils.getRelativeTimeSpanString(dateMillis,
+                System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS).toString();
+
+        return relativeDate;
+    }
+
+    public static Date convertToDate(String date)  {
+
+        Date convertedDate = new Date();
+        SimpleDateFormat sf = new SimpleDateFormat(
+                PARSE_DATE_FORMAT, Locale.US);
+        sf.setLenient(true);
+        try {
+            convertedDate = sf.parse(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return convertedDate;
+    }
 
 
 }
