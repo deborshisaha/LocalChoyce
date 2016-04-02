b722d66d0e3f97e689dd217de8bff2d01fcfb0b5
Deepak Barge <deepak@sumologic.com>
2016-04-02 03:32:38 -0700

    fix the coordinate layout, pageslidingtabs and enable scrolling

--- Android/app/src/main/java/fashiome/android/v2/activities/PanacheHomeActivity.java
+++ Android/app/src/main/java/fashiome/android/v2/activities/PanacheHomeActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
